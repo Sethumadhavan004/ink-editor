47acdb609bbba7f330572ea82d281e189dbf573c
Sethumadhavan004 <nsethu2004@gmail.com>
2026-06-20 10:54:44 +0530

    chore: update demo with parchment theme and full toolbar

--- demo/app/InkEditorClient.tsx
+++ demo/app/InkEditorClient.tsx
@@ -6,5 +6,12 @@ import 'ink-editor/dist/index.css'
 const InkEditor = dynamic(() => import('ink-editor').then((m) => m.InkEditor), { ssr: false })
 
 export function InkEditorClient() {
-  return <InkEditor onChange={(json: object) => console.log(json)} />
+  return (
+    <InkEditor
+      pageSize="A4"
+      theme="parchment"
+      toolbar={['bold', 'italic', 'underline', 'h1', 'h2', 'align', 'list', 'indent']}
+      onChange={(json: object) => console.log(json)}
+    />
+  )
 }
