b5ac0e9512e2a9415ffe99d6c8a62d2f191b1f07
Sethumadhavan004 <nsethu2004@gmail.com>
2026-06-21 09:15:28 +0530

    feat: export FontKey, ThemeColors, PARCHMENT_DEFAULTS, MINIMAL_DEFAULTS from package

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { InkEditor } from './InkEditor'
 export type { InkEditorProps } from './InkEditor'
-export type { PageSize, Theme, ToolbarKey } from './types'
+export type { PageSize, Theme, ToolbarKey, FontKey, ThemeColors } from './types'
+export { PARCHMENT_DEFAULTS, MINIMAL_DEFAULTS } from './types'
